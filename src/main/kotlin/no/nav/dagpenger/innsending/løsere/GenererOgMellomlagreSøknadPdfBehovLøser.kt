d4b6b4f8c1d77fd67350fcc7e576cd2f69642366
ArselanSultani <arselan.sultani@nav.no>
2026-04-16 13:45:34 +0200

    Ignorer søknadIder som feiler i prod

--- src/main/kotlin/no/nav/dagpenger/innsending/løsere/GenererOgMellomlagreSøknadPdfBehovLøser.kt
+++ src/main/kotlin/no/nav/dagpenger/innsending/løsere/GenererOgMellomlagreSøknadPdfBehovLøser.kt
@@ -49,6 +49,9 @@ internal class GenererOgMellomlagreSøknadPdfBehovLøser(
                 "fb774432-3801-4737-8914-fd8af550ff3e",
                 "1eb49db5-8387-4946-9b32-9f9d24caa944",
                 "34a2c4aa-4b2e-49d0-98a7-09dffb806291",
+                "5a2ada17-e1c0-473a-880d-ccaafc637a6f",
+                "e46f72c3-a490-4bab-a29b-e1bf2705bbb5",
+                "25d09010-4e2d-4ce0-bc1e-98917ad84085"
             )
 
         if (søknadId.toString() in ignorerSøknadIder) return
